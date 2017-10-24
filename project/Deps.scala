3740e0948934e58c8c4dcc30f227f01dcbe2ae16
Alexandre Archambault <alexandre.archambault@gmail.com>
2017-10-24 16:31:22 +0200

    Bump dependencies

--- project/Deps.scala
+++ project/Deps.scala
@@ -19,10 +19,10 @@ object Deps {
   def jupyterScalaApi = "org.jupyter-scala" % "scala-api" % jupyterScalaVersion cross CrossVersion.full
   def rhino = "org.mozilla" % "rhino" % "1.7.7.1"
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.2")
-  def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.13" % "1.1.4")
-  def scalajsDom = setting("org.scala-js" %%% "scalajs-dom" % "0.9.1")
-  def scalatags = setting("com.lihaoyi" %%% "scalatags" % "0.6.2")
-  def scalaTest = "org.scalatest" %% "scalatest" % "3.0.1"
-  def utest = setting("com.lihaoyi" %%% "utest" % "0.4.4")
+  def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.13" % "1.1.7")
+  def scalajsDom = setting("org.scala-js" %%% "scalajs-dom" % "0.9.3")
+  def scalatags = setting("com.lihaoyi" %%% "scalatags" % "0.6.7")
+  def scalaTest = "org.scalatest" %% "scalatest" % "3.0.4"
+  def utest = setting("com.lihaoyi" %%% "utest" % "0.5.4")
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,6 @@
-addSbtPlugin("io.get-coursier" % "sbt-coursier" % "1.0.0-RC1")
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.15")
-addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0")
-addSbtPlugin("com.typesafe" % "sbt-mima-plugin" % "0.1.13")
-addSbtPlugin("com.dwijnand" % "sbt-travisci" % "1.1.0")
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.20")
+addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.1.0")
+addSbtPlugin("com.typesafe" % "sbt-mima-plugin" % "0.1.18")
+addSbtPlugin("com.dwijnand" % "sbt-travisci" % "1.1.1")
+
+addSbtCoursier
