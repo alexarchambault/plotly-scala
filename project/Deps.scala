b3de71e042af8d1bd2e97cad4e73d1d3fc12ae3d
Alexandre Archambault <alexandre.archambault@gmail.com>
2017-12-11 17:07:44 +0100

    Update things

--- project/Deps.scala
+++ project/Deps.scala
@@ -15,14 +15,14 @@ object Deps {
   def circeLiteral = setting("io.circe" %% "circe-literal" % circeVersion)
   def circeScalaJs = setting("io.circe" %%% "circe-scalajs" % circeVersion)
   def circeParser = setting("io.circe" %%% "circe-parser" % circeVersion)
-  def jodaTime = "joda-time" % "joda-time" % "2.9.1"
+  def jodaTime = "joda-time" % "joda-time" % "2.9.9"
   def jupyterScalaApi = "org.jupyter-scala" % "scala-api" % jupyterScalaVersion cross CrossVersion.full
-  def rhino = "org.mozilla" % "rhino" % "1.7.7.1"
+  def rhino = "org.mozilla" % "rhino" % "1.7.7.2"
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.2")
   def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.13" % "1.1.7")
-  def scalajsDom = setting("org.scala-js" %%% "scalajs-dom" % "0.9.3")
+  def scalajsDom = setting("org.scala-js" %%% "scalajs-dom" % "0.9.4")
   def scalatags = setting("com.lihaoyi" %%% "scalatags" % "0.6.7")
   def scalaTest = "org.scalatest" %% "scalatest" % "3.0.4"
-  def utest = setting("com.lihaoyi" %%% "utest" % "0.5.4")
+  def utest = setting("com.lihaoyi" %%% "utest" % "0.6.0")
 
 }
