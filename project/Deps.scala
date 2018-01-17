58c4f84c8c1f7b751a90a36828c340c6c623c556
Felix Dietze <felix.dietze@rwth-aachen.de>
2018-01-17 12:14:05 +0100

    Update dependencies

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,7 +7,7 @@ object Deps {
 
   import Def.setting
 
-  private val jupyterScalaVersion = "0.4.0"
+  private val jupyterScalaVersion = "0.4.2"
   private val circeVersion = "0.9.0"
 
 
@@ -18,11 +18,11 @@ object Deps {
   def jodaTime = "joda-time" % "joda-time" % "2.9.1"
   def jupyterScalaApi = "org.jupyter-scala" % "scala-api" % jupyterScalaVersion cross CrossVersion.full
   def rhino = "org.mozilla" % "rhino" % "1.7.7.1"
-  def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.2")
+  def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.3")
   def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.13" % "1.1.7")
   def scalajsDom = setting("org.scala-js" %%% "scalajs-dom" % "0.9.3")
   def scalatags = setting("com.lihaoyi" %%% "scalatags" % "0.6.7")
   def scalaTest = "org.scalatest" %% "scalatest" % "3.0.4"
-  def utest = setting("com.lihaoyi" %%% "utest" % "0.5.4")
+  def utest = setting("com.lihaoyi" %%% "utest" % "0.6.3")
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.20")
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.21")
 addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.1.0")
 addSbtPlugin("com.typesafe" % "sbt-mima-plugin" % "0.1.18")
 addSbtPlugin("com.dwijnand" % "sbt-travisci" % "1.1.1")
