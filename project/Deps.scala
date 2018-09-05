0527966f20749cd86f4e56ab1346fc426f1875d1
Alexandre Archambault <alexandre.archambault@gmail.com>
2018-09-05 15:54:42 +0200

    Switch to almond

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,16 +7,15 @@ object Deps {
 
   import Def.setting
 
-  private val jupyterScalaVersion = "0.4.0"
   private val circeVersion = "0.9.1"
 
 
+  def almondScalaApi = "sh.almond" % "scala-kernel-api" % "0.1.5" cross CrossVersion.full
   def circeCore = setting("io.circe" %%% "circe-core" % circeVersion)
   def circeLiteral = setting("io.circe" %% "circe-literal" % circeVersion)
   def circeScalaJs = setting("io.circe" %%% "circe-scalajs" % circeVersion)
   def circeParser = setting("io.circe" %%% "circe-parser" % circeVersion)
   def jodaTime = "joda-time" % "joda-time" % "2.9.1"
-  def jupyterScalaApi = "org.jupyter-scala" % "scala-api" % jupyterScalaVersion cross CrossVersion.full
   def rhino = "org.mozilla" % "rhino" % "1.7.7.1"
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.2")
   def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.13" % "1.1.7")
