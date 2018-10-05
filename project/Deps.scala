bf37c9cb475bd0c88fddb6df1f75e4a5a1bc39c8
Alexandre Archambault <alexandre.archambault@gmail.com>
2018-10-05 12:10:35 +0200

    Bump dependencies

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,15 +7,15 @@ object Deps {
 
   import Def.setting
 
-  def almondScalaApi = "sh.almond" % "scala-kernel-api" % "0.1.5" cross CrossVersion.full
+  def almondScalaApi = "sh.almond" % "scala-kernel-api" % "0.1.9" cross CrossVersion.full
   def argonautShapeless = setting("com.github.alexarchambault" %%% "argonaut-shapeless_6.2" % "1.2.0-M8")
   def jodaTime = "joda-time" % "joda-time" % "2.9.1"
   def rhino = "org.mozilla" % "rhino" % "1.7.7.1"
-  def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.2")
-  def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.13" % "1.1.7")
-  def scalajsDom = setting("org.scala-js" %%% "scalajs-dom" % "0.9.3")
+  def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.3")
+  def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.14" % "1.2.0-1")
+  def scalajsDom = setting("org.scala-js" %%% "scalajs-dom" % "0.9.6")
   def scalatags = setting("com.lihaoyi" %%% "scalatags" % "0.6.7")
-  def scalaTest = "org.scalatest" %% "scalatest" % "3.0.4"
-  def utest = setting("com.lihaoyi" %%% "utest" % "0.5.4")
+  def scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  def utest = setting("com.lihaoyi" %%% "utest" % "0.6.6")
 
 }
