a631e6a65d48374c4014ec30e1472436ee59e908
Alexandre Archambault <alexandre.archambault@gmail.com>
2018-12-17 17:12:21 +0100

    Switch to scala 2.12.8

--- project/Settings.scala
+++ project/Settings.scala
@@ -85,7 +85,7 @@ object Settings {
     sourceGenerators.in(Compile) += customSourceGenerators.taskValue
   )
 
-  private val scala212 = "2.12.7"
+  private val scala212 = "2.12.8"
   private val scala211 = "2.11.12"
 
   lazy val shared = Seq(
