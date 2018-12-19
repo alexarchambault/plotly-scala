30db433cc5de0a3d3a044e12e8c6f61dc958283e
Scala steward <me@scala-steward.org>
2018-12-19 20:23:39 +0100

    Update prism to 1.5.1

--- project/WebDeps.scala
+++ project/WebDeps.scala
@@ -10,6 +10,6 @@ object WebDeps {
   def bootstrap = "org.webjars.bower" % "bootstrap" % "3.3.6"
   def jquery = "org.webjars.bower" % "jquery" % "2.2.4"
   def plotlyJs = "org.webjars.bower" % "plotly.js" % Versions.plotlyJs
-  def prism = "org.webjars.bower" % "prism" % "1.5.0"
+  def prism = "org.webjars.bower" % "prism" % "1.5.1"
 
 }
