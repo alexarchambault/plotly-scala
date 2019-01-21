aea64b637ead444fc5e6ee78e769861f7a0abf18
Scala steward <43047562+scala-steward@users.noreply.github.com>
2019-01-21 23:09:40 +0100

    Update bootstrap to 3.3.7 (#50)

--- project/WebDeps.scala
+++ project/WebDeps.scala
@@ -7,7 +7,7 @@ object WebDeps {
     def plotlyJs = "1.41.3"
   }
 
-  def bootstrap = "org.webjars.bower" % "bootstrap" % "3.3.6"
+  def bootstrap = "org.webjars.bower" % "bootstrap" % "3.3.7"
   def jquery = "org.webjars.bower" % "jquery" % "2.2.4"
   def plotlyJs = "org.webjars.bower" % "plotly.js" % Versions.plotlyJs
   def prism = "org.webjars.bower" % "prism" % "1.5.0"
