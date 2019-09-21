5b51bab363a8f408f113f80da091a8e9721a8421
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-09-21 15:53:43 +0200

    Switch to scalatags 0.7.0

--- project/Deps.scala
+++ project/Deps.scala
@@ -14,7 +14,7 @@ object Deps {
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.3")
   def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.14" % "1.2.0-1")
   def scalajsDom = setting("org.scala-js" %%% "scalajs-dom" % "0.9.7")
-  def scalatags = setting("com.lihaoyi" %%% "scalatags" % "0.6.8")
+  def scalatags = setting("com.lihaoyi" %%% "scalatags" % "0.7.0")
   def scalaTest = "org.scalatest" %% "scalatest" % "3.0.8"
   def utest = setting("com.lihaoyi" %%% "utest" % "0.6.6")
 
