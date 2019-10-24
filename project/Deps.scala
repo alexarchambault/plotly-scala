6ed34ab1708bd6f7a9461b8e40874ed2603ba995
Scala Steward <me@scala-steward.org>
2019-10-24 20:12:40 +0200

    Update joda-time to 2.10.5

--- project/Deps.scala
+++ project/Deps.scala
@@ -9,7 +9,7 @@ object Deps {
 
   def almondScalaApi = "sh.almond" %% "jupyter-api" % "0.8.2"
   def argonautShapeless = setting("com.github.alexarchambault" %%% "argonaut-shapeless_6.2" % "1.2.0-M11")
-  def jodaTime = "joda-time" % "joda-time" % "2.10.4"
+  def jodaTime = "joda-time" % "joda-time" % "2.10.5"
   def rhino = "org.mozilla" % "rhino" % "1.7.11"
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.3")
   def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.14" % "1.2.0-1")
