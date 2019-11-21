3eb6ed7eb03143fde6b2d060ec3ebbee45646cc2
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-11-21 09:07:04 +0100

    Update jupyter-api to 0.8.3 (#130)

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,7 +7,7 @@ object Deps {
 
   import Def.setting
 
-  def almondScalaApi = "sh.almond" %% "jupyter-api" % "0.8.2"
+  def almondScalaApi = "sh.almond" %% "jupyter-api" % "0.8.3"
   def argonautShapeless = setting("com.github.alexarchambault" %%% "argonaut-shapeless_6.2" % "1.2.0-M11")
   def dataClass = "io.github.alexarchambault" %% "data-class" % "0.2.0"
   def jodaTime = "joda-time" % "joda-time" % "2.10.4"
