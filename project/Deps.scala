6141c31e4d085d5a56807d92589e5c39649a90e9
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-12-07 16:31:01 +0100

    Update scalajs-dom to 0.9.8 (#139)

--- project/Deps.scala
+++ project/Deps.scala
@@ -14,7 +14,7 @@ object Deps {
   def rhino = "org.mozilla" % "rhino" % "1.7.11"
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.3")
   def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.14" % "1.2.0-1")
-  def scalajsDom = setting("org.scala-js" %%% "scalajs-dom" % "0.9.7")
+  def scalajsDom = setting("org.scala-js" %%% "scalajs-dom" % "0.9.8")
   def scalatags = setting("com.lihaoyi" %%% "scalatags" % "0.7.0")
   def scalaTest = "org.scalatest" %% "scalatest" % "3.0.8"
   def utest = setting("com.lihaoyi" %%% "utest" % "0.6.6")
