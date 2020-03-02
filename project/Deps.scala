be5982f85796f9f37779dbd67d962da839118395
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-03-02 16:20:27 +0100

    Update argonaut-shapeless_6.2 to 1.2.0-M12 (#168)

--- project/Deps.scala
+++ project/Deps.scala
@@ -8,7 +8,7 @@ object Deps {
   import Def.setting
 
   def almondScalaApi = "sh.almond" %% "jupyter-api" % "0.9.1"
-  def argonautShapeless = setting("com.github.alexarchambault" %%% "argonaut-shapeless_6.2" % "1.2.0-M11")
+  def argonautShapeless = setting("com.github.alexarchambault" %%% "argonaut-shapeless_6.2" % "1.2.0-M12")
   def dataClass = "io.github.alexarchambault" %% "data-class" % "0.2.1"
   def jodaTime = "joda-time" % "joda-time" % "2.10.5"
   def rhino = "org.mozilla" % "rhino" % "1.7.12"
