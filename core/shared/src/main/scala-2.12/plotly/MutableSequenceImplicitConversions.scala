154dc75bd10fac9beae36c92a251e00e0c8cbef6
Timothy McCarthy <timothy.mccarthy@rea-group.com>
2020-04-19 22:22:26 +1000

    Fix version-specific directory naming

--- core/shared/src/main/scala-2.12/plotly/MutableSequenceImplicitConversions.scala
+++ core/shared/src/main/scala-2.12/plotly/MutableSequenceImplicitConversions.scala
@@ -1,9 +1,5 @@
 package plotly
 
-import plotly.Sequence.{DateTimes, Doubles, NestedDoubles, NestedInts, Strings}
-import plotly.element.LocalDateTime
-import scala.collection.{Seq => BaseScalaSeq}
-
 trait MutableSequenceImplicitConversions {
   // Unneccessary in Scala 2.12, since the `Seq` alias refers to the supertype for mutable and immutable sequences
 }
