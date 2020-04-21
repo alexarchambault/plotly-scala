ccaa176c0382fb78a05caf4551ca574a65a9a2d2
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-04-21 18:24:27 +0200

    Update bootstrap to 3.4.0

--- project/WebDeps.scala
+++ project/WebDeps.scala
@@ -7,7 +7,7 @@ object WebDeps {
     def plotlyJs = "1.52.2"
   }
 
-  def bootstrap = "org.webjars.bower" % "bootstrap" % "3.3.7"
+  def bootstrap = "org.webjars.bower" % "bootstrap" % "3.4.0"
   def jquery = "org.webjars.bower" % "jquery" % "2.2.4"
   def plotlyJs = "org.webjars.bower" % "plotly.js" % Versions.plotlyJs
   def prism = "org.webjars.bower" % "prism" % "1.5.1"
