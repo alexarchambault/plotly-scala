ae7019d62d1fb552389c31eb6d197594bdbd7247
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-04-27 18:18:24 +0200

    Update scala to 2.12.11

--- project/Settings.scala
+++ project/Settings.scala
@@ -87,7 +87,7 @@ object Settings {
     sourceGenerators.in(Compile) += customSourceGenerators.taskValue
   )
 
-  private val scala212 = "2.12.10"
+  private val scala212 = "2.12.11"
   private val scala213 = "2.13.1"
 
   private lazy val isAtLeastScala213 = Def.setting {
