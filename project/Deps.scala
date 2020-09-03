cfb1d4b3b20ddaa948c166dffae796d5038f41b9
Scala Steward <me@scala-steward.org>
2020-09-03 06:05:45 +0200

    Update rhino to 1.7.13

--- project/Deps.scala
+++ project/Deps.scala
@@ -11,7 +11,7 @@ object Deps {
   def argonautShapeless = setting("com.github.alexarchambault" %%% "argonaut-shapeless_6.2" % "1.2.0")
   def dataClass = "io.github.alexarchambault" %% "data-class" % "0.2.3"
   def jodaTime = "joda-time" % "joda-time" % "2.10.6"
-  def rhino = "org.mozilla" % "rhino" % "1.7.12"
+  def rhino = "org.mozilla" % "rhino" % "1.7.13"
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.3")
   def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.14" % "1.2.0-1")
   def scalajsDom = setting("org.scala-js" %%% "scalajs-dom" % "1.1.0")
