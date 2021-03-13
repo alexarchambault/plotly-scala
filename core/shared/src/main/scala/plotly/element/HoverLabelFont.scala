e1101c4ea735b29f5a164d45f961c05b8873db36
Alexey Novakov <novakov.alex@gmail.com>
2021-03-13 20:11:25 +0100

    add Image support

---
+++ core/shared/src/main/scala/plotly/element/HoverLabelFont.scala
@@ -0,0 +1,9 @@
+package plotly.element
+
+import dataclass.data
+
+@data(optionSetters = true) class HoverLabelFont(
+  family: Option[OneOrSeq[String]] = None,
+  size: Option[OneOrSeq[Double]] = None,
+  color: Option[OneOrSeq[Color]] = None
+)
