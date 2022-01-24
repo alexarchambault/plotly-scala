c73e59b10d60366284b005da287cab1a7eee8638
Scala Steward <me@scala-steward.org>
2022-01-24 12:08:37 +0100

    Update scalatest to 3.2.11

--- project/Deps.scala
+++ project/Deps.scala
@@ -16,6 +16,6 @@ object Deps {
   def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.15" % "1.3.0")
   def scalajsDom = setting("org.scala-js" %%% "scalajs-dom" % "1.1.0")
   def scalatags = setting("com.lihaoyi" %%% "scalatags" % "0.9.4")
-  def scalaTest = "org.scalatest" %% "scalatest" % "3.2.9"
+  def scalaTest = "org.scalatest" %% "scalatest" % "3.2.11"
 
 }
