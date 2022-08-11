2067e068b160a7a46b8062ecbbfe92b7d1e5c8e0
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-08-11 11:18:51 +0200

    Update scalatags to 0.11.1 (#327)

--- project/Deps.scala
+++ project/Deps.scala
@@ -15,7 +15,7 @@ object Deps {
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.7")
   def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.15" % "1.3.0")
   def scalajsDom = setting("org.scala-js" %%% "scalajs-dom" % "1.2.0")
-  def scalatags = setting("com.lihaoyi" %%% "scalatags" % "0.9.4")
+  def scalatags = setting("com.lihaoyi" %%% "scalatags" % "0.11.1")
   def scalaTest = "org.scalatest" %% "scalatest" % "3.2.13"
 
 }
