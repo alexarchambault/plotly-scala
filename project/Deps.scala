2b6ce66bbcab1c41c1d7b56d74b92d0ee043dbb7
Sven Behrens <sven.behrens@usu.com>
2024-04-27 14:55:46 +0200

    Update scala deps

--- project/Deps.scala
+++ project/Deps.scala
@@ -7,15 +7,15 @@ object Deps {
 
   import Def.setting
 
-  def almondScalaApi = "sh.almond" %% "jupyter-api" % "0.13.4"
+  def almondScalaApi = "sh.almond" %% "jupyter-api" % "0.13.14"
   def argonautShapeless = setting("com.github.alexarchambault" %%% "argonaut-shapeless_6.3" % "1.3.1")
   def dataClass = "io.github.alexarchambault" %% "data-class" % "0.2.6"
-  def jodaTime = "joda-time" % "joda-time" % "2.12.4"
+  def jodaTime = "joda-time" % "joda-time" % "2.12.7"
   def rhino = "org.mozilla" % "rhino" % "1.7.14"
   def shapeless = setting("com.chuusai" %%% "shapeless" % "2.3.7")
   def scalacheckShapeless = setting("com.github.alexarchambault" %%% "scalacheck-shapeless_1.15" % "1.3.0")
-  def scalajsDom = setting("org.scala-js" %%% "scalajs-dom" % "2.4.0")
-  def scalatags = setting("com.lihaoyi" %%% "scalatags" % "0.12.0")
-  def scalaTest = "org.scalatest" %% "scalatest" % "3.2.15"
+  def scalajsDom = setting("org.scala-js" %%% "scalajs-dom" % "2.8.0")
+  def scalatags = setting("com.lihaoyi" %%% "scalatags" % "0.13.1")
+  def scalaTest = "org.scalatest" %% "scalatest" % "3.2.18"
 
 }
