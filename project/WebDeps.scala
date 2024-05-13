d187a2559f58182d53500a33249e9f266242ca67
Scala Steward <scala_steward@virtuslab.com>
2024-05-13 13:19:14 +0000

    Update jquery to 3.7.1

--- project/WebDeps.scala
+++ project/WebDeps.scala
@@ -7,7 +7,7 @@ object WebDeps {
   }
 
   def bootstrap = "org.webjars.bower" % "bootstrap" % "3.4.1"
-  def jquery    = "org.webjars.bower" % "jquery"    % "3.6.3"
+  def jquery    = "org.webjars.bower" % "jquery"    % "3.7.1"
   def plotlyJs  = "org.webjars.bower" % "plotly.js" % Versions.plotlyJs
   def prism     = "org.webjars.bower" % "prism"     % "1.16.0"
 
